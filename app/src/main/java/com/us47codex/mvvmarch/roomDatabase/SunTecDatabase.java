6fdc6e962bcd81e845e40c01fb2725224572b372
Upen <upendrashah47@gmail.com>
2019-08-24 22:17:11 +0530

    DB related changes

--- app/src/main/java/com/us47codex/mvvmarch/roomDatabase/SunTecDatabase.java
+++ app/src/main/java/com/us47codex/mvvmarch/roomDatabase/SunTecDatabase.java
@@ -1,10 +1,44 @@
 package com.us47codex.mvvmarch.roomDatabase;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 /**
  * Created by Upendra Shah on 24 August, 2019 for
  * Project : MVVM-Architecture
  * Company : US47Codex
  * Email : us47codex@gmail.com
  **/
-public class SunTecDatabase {
+
+@Database(entities = {User.class}, version = 2)
+@TypeConverters(Converter.class)
+public abstract class SunTecDatabase extends RoomDatabase {
+
+    private static SunTecDatabase INSTANCE;
+
+    public abstract UserDao userDao();
+
+    public static SunTecDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), SunTecDatabase.class, "aavgo_guest_db")
+//                    .allowMainThreadQueries()  // allow queries on the main thread. Don't do this on a real app! See PersistenceBasicSample for an example.
+//                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3) // Add migration for upgrading db version
+                    .fallbackToDestructiveMigration() // Gracefully handle missing migration paths :: https://developer.android.com/training/data-storage/room/migrating-db-versions.html#handle-missing-migrations
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+// Since we didn't alter the table, there's nothing else to do here.
+        }
+    };
 }
